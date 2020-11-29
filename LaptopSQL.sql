a442e7ece4a30a29b415ea557b91309b169bd788
Ducphu-dev <53066998+Ducphu-dev@users.noreply.github.com>
2020-11-29 14:33:38 +0700

    Update LaptopSQL.sql

--- LaptopSQL.sql
+++ LaptopSQL.sql
@@ -19,8 +19,6 @@ CREATE TABLE Laptop (
 );
 
 insert into Laptop values ('L101','ASUS TUF FX505DY',15,'1080 x 1920px',144,'2,2 Kg',360,262,25,'Window 10 HOME','Ryzen 5 - 3550H 2.10 GHz boot clock 3.50GHz 4 cores 8 threads','8GB DDR4 2666MZ','GTX 1650 TI 4GB','128 SSB M.2 MVMe 2500MB/s','Wi-Fi 5 (802.11 ac)','6 Cell 48WHrs','2x USB 2.0, 1x USB 3.0, 1x Type C USB 3.1, HDMI');
-
 insert into Laptop values ('L102','Dell XPS 15',15,'1080 x 1920px',60,'1.2 Kg',360,262,25,'Window 10 HOME','Core I5-10500H 3.50GHz boot clock 3,80 GHz 6 core 10 threads','16GB DDR4 2666MZ','GTX 1650 4GB','256 SSB M.2 MVMe 2500MB/s','Wi-Fi 6 (802.11 ac)','10 Cell 65WHrs','2x USB 2.0, 1x USB 3.0, 1x Type C USB 3.1, HDMI');
 
 select * from Laptop 
-DELETE FROM Laptop WHERE ProductID = 'L101';
